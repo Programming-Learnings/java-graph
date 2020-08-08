b01abf17517b348ad24de6c222901d26c7c9deb1
rahulbreezo <rahul@breezo.in>
2020-08-08 14:59:46 +0530

    complete

--- motherVertex.java
+++ motherVertex.java
@@ -8,9 +8,8 @@ class Graph {
     Graph(int v) {
         V = v;
         adj = new LinkedList[v];
-        for (int i = 0; i < v; i++) {
+        for (int i = 0; i < v; ++i)
             adj[i] = new LinkedList();
-        }
     }
 
     void addEdge(int v, int w) {
@@ -19,38 +18,56 @@ class Graph {
 
     void DFSUtil(int v, boolean visited[]) {
         visited[v] = true;
-        System.out.println(v);
 
         Iterator<Integer> i = adj[v].listIterator();
         while (i.hasNext()) {
             int n = i.next();
-            if (!visited[n]) {
+            if (!visited[n])
                 DFSUtil(n, visited);
+        }
+    }
+
+    void DFS() {
+        boolean visited[] = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            visited[i] = false;
+        }
+
+        int v = -1;
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == false) {
+                DFSUtil(i, visited);
+                v = i;
             }
         }
 
-    }
+        for (int i = 0; i < V; i++) {
+            visited[i] = false;
+        }
 
-    void DFS(int v) {
-
-        boolean visited[] = new boolean[V];
+        System.out.println(" Mother Vertex Detected At:" + v);
         DFSUtil(v, visited);
-
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == false) {
+                System.out.println("Not a mother vertex.Node that wasn't connected :" + i);
+                return;
+            }
+        }
+        System.out.println(" Mother Vertex: " + v);
     }
 
     public static void main(String args[]) {
-        Graph g = new Graph(4);
+        Graph g = new Graph(7);
+
         g.addEdge(0, 1);
         g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 3);
-
-        System.out.println("Following is Depth First Traversal " + "(starting from vertex 2)");
-
-        g.DFS(2);
-
+        g.addEdge(1, 3);
+        g.addEdge(4, 1);
+        g.addEdge(6, 4);
+        g.addEdge(6, 0);
+        g.addEdge(5, 2);
+        g.addEdge(5, 6);
+        g.DFS();
     }
 
 }
